dbf742696d7d56cc868d2cb557dd72a1703ba426
tobias970 <tobias-97@outlook.dk>
2019-03-19 15:00:32 +0100

    Completed (Banner,News,Company,Info)

---
+++ public_html/contact.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tubbe
+ * Date: 19-03-2019
+ * Time: 10:15
+ */
--- public_html/index.php
+++ public_html/index.php
@@ -4,9 +4,146 @@
         <meta charset="UTF-8">
         <title>Hjem</title>
         <link rel="stylesheet" href="libs/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
-        <p>Home</p>
+
+    <!-- Using banner from banner.html -->
+
+
+
+
+    <!-- Using navbar from navbar.html -->
+    <div class="main container-fluid">
+        <?php require_once "../templates/banner.html"; ?>
+        <?php require_once "../templates/navbar.html"; ?>
+
+        <p style="width:10px;">&nbsp;</p>
+    <div class="card-deck">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <p style="height: 20px;">&nbsp;</p>
+
+    <div class="infoimg container">
+        <div class="row">
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <img src="/assets/Trust.png" alt="Trust">
+                    <div class="card-body"> Trust </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <img src="/assets/Respect.png" alt="Respect">
+                    <div class="card-body"> Respect </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <img src="/assets/SecureConnection.png" alt="Secure Connection">
+                    <div class="card-body"> Secure Connection </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <img src="/assets/Growth.png" alt="Growth">
+                    <div class="card-body"> Growth </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+    <!-- Footer -->
+    <footer class="page-footer font-small bg-primary">
+        <!-- Footer Links -->
+        <div class="container-fluid text-center text-md-left">
+            <!-- Grid row -->
+            <div class="row">
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3">
+                    <!-- Content -->
+                    <h5 class="text-uppercase">Footer Content</h5>
+                    <p>Here you can use rows and columns here to organize your footer content.</p>
+                </div>
+                <!-- Grid column -->
+                <hr class="clearfix w-100 d-md-none pb-3">
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <!-- Links -->
+                    <h5 class="text-uppercase">Links</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+
+                    <!-- Links -->
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 4</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+        <!-- Footer Links -->
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+
     </body>
 </html>
 <script src="libs/jquery/jquery-3.3.1.min.js"></script>
