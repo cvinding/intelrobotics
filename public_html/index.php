5a13bd0078290f3c92b8d802347a436ceadd3f5c
czvr0 <chri656v@edu.sde.dk>
2019-03-19 09:59:43 +0100

    beginning of external website

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="dk">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hjem</title>
+        <link rel="stylesheet" href="libs/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <p>Home</p>
+    </body>
+</html>
+<script src="libs/jquery/jquery-3.3.1.min.js"></script>
+<script src="libs/popper/popper.min.js"></script>
+<script src="libs/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
