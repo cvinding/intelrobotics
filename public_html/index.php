fbe8f5fbf27a6fce0fe4164c00bfe812734c7172
tobias970 <tobias-97@outlook.dk>
2019-03-20 08:31:56 +0100

    Completed (Banner,News,Company,Info)

--- public_html/index.php
+++ public_html/index.php
@@ -75,43 +75,31 @@
 
 
     </div>
+    <p style="height:30px;">&nbsp;</p>
     <!-- Footer -->
-    <footer class="page-footer font-small bg-primary">
+    <footer class="page-footer font-small bg-dark text-white">
+
         <!-- Footer Links -->
         <div class="container-fluid text-center text-md-left">
+
             <!-- Grid row -->
             <div class="row">
+
                 <!-- Grid column -->
-                <div class="col-md-6 mt-md-0 mt-3">
+                <div class="col-md-6 mt-md-5 mt-3">
+
                     <!-- Content -->
                     <h5 class="text-uppercase">Footer Content</h5>
                     <p>Here you can use rows and columns here to organize your footer content.</p>
+
                 </div>
                 <!-- Grid column -->
+
                 <hr class="clearfix w-100 d-md-none pb-3">
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
-                    <!-- Links -->
-                    <h5 class="text-uppercase">Links</h5>
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!">Link 1</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 2</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 3</a>
-                        </li>
-                        <li>
-                            <a href="#!">Link 4</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- Grid column -->
-                <!-- Grid column -->
-                <div class="col-md-3 mb-md-0 mb-3">
 
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <p style="height:10px;">&nbsp;</p>
                     <!-- Links -->
                     <h5 class="text-uppercase">Links</h5>
 
@@ -132,15 +120,43 @@
 
                 </div>
                 <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <p style="height:10px;">&nbsp;</p>
+                    <!-- Links -->
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!">Link 1</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 2</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!">Link 4</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- Grid column -->
+
             </div>
             <!-- Grid row -->
+
         </div>
         <!-- Footer Links -->
+
         <!-- Copyright -->
         <div class="footer-copyright text-center py-3">© 2018 Copyright:
             <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
         </div>
         <!-- Copyright -->
+
     </footer>
     <!-- Footer -->
 
